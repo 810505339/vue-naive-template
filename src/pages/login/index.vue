97e092c0534d2bbed2c53adad3cd924a37bec63b
xueming <810505339@qq.com>
2022-12-12 10:27:48 +0800

    添加微信api 添加axios

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,6 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+onMounted(() => {
+  nextTick(() => {
+    const obj = new WxLogin({
+      self_redirect: true,
+      id: 'login_container',
+      appid: 'wx1b1b3d3c419f666d',
+      scope: 'snsapi_login',
+      redirect_uri: encodeURIComponent('www.baidu.com'),
+      style: 'black',
+    })
+  })
+})
+</script>
 
 <template>
+  <div id="login_container" />
   login
 </template>
 
