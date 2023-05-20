6af4844be23e56db876265b10c7cff6faf7f1224
xueming <810505339@qq.com>
2023-05-20 14:55:00 +0800

    edit .env

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,27 +1,8 @@
 <script setup lang="ts">
-const router = useRouter()
-onMounted(() => {
-  nextTick(() => {
-    // wxe60924bc3543a091
-    const obj = new WxLogin({
-      self_redirect: false,
-      id: 'login_container',
-      appid: 'wxbdc5610cc59c1631',
-      scope: 'snsapi_login',
-      redirect_uri: 'https%3A%2F%2Fpassport.yhd.com%2Fwechat%2Fcallback.do',
-      style: 'black',
-      state: Math.random(),
-    })
-  })
-})
+
 </script>
 
-<template>
-  <n-button type="primary" @click="router.back()">
-    返回
-  </n-button>
-  <div id="login_container" />
-</template>
+<template />
 
 <route lang="yaml">
 meta:
