9ecb2ad5bdcf53da673942e54bafd13764ee0891
xueming <810505339@qq.com>
2023-05-26 17:25:51 +0800

    add name

--- src/components/playList/index.vue
+++ src/components/playList/index.vue
@@ -1,97 +1,95 @@
 <script setup lang="ts">
 import { instance } from '~/api'
 import dayjs from 'dayjs'
-import usePlayingList,{PlayModel} from '~/store/modules/playingList';
+import usePlayingList, { PlayModel } from '~/store/modules/playingList';
 // 热歌榜
 const { data } = useAxios('/playlist/detail?id=3778678', instance)
 const playList = computed(() => {
   return data.value?.playlist?.tracks.slice(0, 4)
 })
-function durationStr(dt:number){
+function durationStr(dt: number) {
   return dayjs(dt).format('mm:ss')
 }
 
 const playListStore = usePlayingList()
 const audio = ref()
-const src=ref(playListStore.selectItem?.url)
+const src = ref(playListStore.selectItem?.url)
 //切换歌曲
-const { playing, currentTime, duration, volume } = useMediaControls(audio, { 
-  src:src
+const { playing, currentTime, duration, volume } = useMediaControls(audio, {
+  src: src
 })
 
-watch(()=>playListStore.selectIndex,()=>{
- 
-  playing.value=false
-  src.value=playListStore.selectItem.url
-  setTimeout(()=>{
-    playing.value=true
-  },200)
-  
+watch(() => playListStore.selectIndex, () => {
+
+  playing.value = false
+  src.value = playListStore.selectItem.url
+  setTimeout(() => {
+    playing.value = true
+  }, 200)
+
 })
 
 //时长进度条
-const slider=ref(0)
+const slider = ref(0)
 
 //点击播放跟暂停
-function changePlay(){
+function changePlay() {
   playing.value = !playing.value
 }
 
 //修改模式
-function changeModel(){
-  playListStore.playmodel=playListStore.playmodel===2? 0:playListStore.playmodel+1
+function changeModel() {
+  playListStore.playmodel = playListStore.playmodel === 2 ? 0 : playListStore.playmodel + 1
 }
 //点击拉动歌曲进度条
-function updateSlider(value: number){
+function updateSlider(value: number) {
 
-  slider.value=value
-  currentTime.value=(value*duration.value)/100
-  playing.value=true
+  slider.value = value
+  currentTime.value = (value * duration.value) / 100
+  playing.value = true
 }
 //点击top100单曲
-async function playMusic(play:any){
+async function playMusic(play: any) {
   playListStore.playSong(play)
-  currentTime.value=0;
-  playing.value=true;
+  currentTime.value = 0;
+  playing.value = true;
 }
 
-const playIcon=computed(()=>{
-  return !playing.value?`i-ic:baseline-play-arrow`:`i-ic:baseline-pause`
+const playIcon = computed(() => {
+  return !playing.value ? `i-ic:baseline-play-arrow` : `i-ic:baseline-pause`
 })
 
 //正在播放时长显示
-const currentTimeFormat=computed(()=>{
+const currentTimeFormat = computed(() => {
   return dayjs.unix(currentTime.value).format('mm:ss')
 })
 //总时长显示
-const durationFormat=computed(()=>{
+const durationFormat = computed(() => {
   return dayjs.unix(duration.value).format('mm:ss')
 })
 //播放模式显示
-const playModelFormat=computed(()=>{
-  const playModelList=["随机播放","循环播放","单曲循环"]
-    return playModelList[playListStore.playmodel]
+const playModelFormat = computed(() => {
+  const playModelList = ["随机播放", "循环播放", "单曲循环"]
+  return playModelList[playListStore.playmodel]
 })
 
-const playModelIcon=computed(()=>{
-  const playModelIconList=["i-ic:sharp-shuffle","i-ic:outline-repeat","i-ic:baseline-repeat-one"]
-    return playModelIconList[playListStore.playmodel]
+const playModelIcon = computed(() => {
+  const playModelIconList = ["i-ic:sharp-shuffle", "i-ic:outline-repeat", "i-ic:baseline-repeat-one"]
+  return playModelIconList[playListStore.playmodel]
 })
 
 //监听进度条
-watch(()=>currentTime.value,()=>{
-  slider.value=(currentTime.value/duration.value) *100
+watch(() => currentTime.value, () => {
+  slider.value = (currentTime.value / duration.value) * 100
   //如果播放完毕
-  if(currentTime.value===duration.value)
-  {
+  if (currentTime.value === duration.value) {
     //如果是单曲循环就再次播放
-    if(playListStore.playmodel===PlayModel.loop)
-    {
-      currentTime.value=0
-      setTimeout(()=>{
-        playing.value=true
-      },200)
-      return 
+    if (playListStore.playmodel === PlayModel.loop) {
+      currentTime.value = 0
+      setTimeout(() => {
+        playing.value = true
+      }, 200)
+      return
     }
     playListStore.next()
   }
@@ -102,91 +100,89 @@ watch(()=>currentTime.value,()=>{
 </script>
 
 <template>
-  
-  
-  
   <audio ref="audio" hidden></audio>
-  <div fixed z-10 w="400px" right="0" top="80px" rounded-tl-3xl h="100%" bg=" white dark:#18181c"  pt="20px">
-    <div >
+  <div fixed z-10 w="400px" right="0" top="80px" rounded-tl-3xl h="100%" bg=" white dark:#18181c" pt="20px">
+    <div>
       <div flex items-center justify-between px="24px">
-      <span text-18px font-600>TOP 100 WEEKLY</span>
-      <span text-14px text="#787887">MORE LIST</span>
-    </div>
-    <div my8px>
-      <div v-for="(play,i) in playList" :key="play.id" flex items-center w="100%" justify-evenly py-8px px="24px" hover="bg-gray-50 dark:bg-gray-2/3" @click="playMusic(play)">
-        <span mr-15px text="#787887">0{{ i+1 }}</span>
-        <img
-          :src="play.al.picUrl" w="50px" h="50px" mr-8px
-          rounded
-        >
-        <div basis="1/2">
-          <div font-600 text-16px truncate>
-            {{play.name}}
-          </div>
-          <div text="#787887 13px" mt-3px truncate>
-            {{play.ar.map((p:any)=>p.name).join('/')}}
-          </div>
-        </div>
-        <span text-13px text="#787887">{{durationStr(play.dt)}}</span>
-        <i icon-btn i-heroicons:ellipsis-vertical-20-solid text-18px font-600 />
+        <span text-18px font-600>TOP 100 WEEKLY</span>
+        <span text-14px text="#787887">MORE LIST</span>
       </div>
-    </div>
+      <div my8px>
+        <div v-for="(play, i) in playList" :key="play.id" flex items-center w="100%" justify-evenly py-8px px="24px"
+          hover="bg-gray-50 dark:bg-gray-2/3" @click="playMusic(play)">
+          <span mr-15px text="#787887">0{{ i + 1 }}</span>
+          <img :src="play.al.picUrl" w="50px" h="50px" mr-8px rounded>
+          <div basis="1/2">
+            <div font-600 text-16px truncate>
+              {{ play.name }}
+            </div>
+            <div text="#787887 13px" mt-3px truncate>
+              {{ play.ar.map((p: any) => p.name).join('/') }}
+            </div>
+          </div>
+          <span text-13px text="#787887">{{ durationStr(play.dt) }}</span>
+          <i icon-btn i-heroicons:ellipsis-vertical-20-solid text-18px font-600 />
+        </div>
+      </div>
     </div>
 
     <div px="24px">
-      <div font-500 text-18px  text="#787887 center" mb-10px >NOW PLAYING</div>
+      <div font-500 text-18px text="#787887 center" mb-10px>NOW PLAYING</div>
       <div text-center>
-        <img rounded-16px w="200px" h="120px" inline-block blur="0.5"  :src="playListStore.selectItem?.al?.picUrl" v-if="playListStore.selectItem?.al?.picUrl"/>
-        <div text="#24242e dark:white 24px" font-700 my-4px>{{playListStore.selectItem?.name}}</div>
-        <div text="#4d4d56 16px" font-500 mb-8px>{{playListStore.selectItem?.ar?.map((p:any)=>p.name).join('/')}}</div> 
-        <n-slider  :tooltip="false" v-model:value="slider" :on-update:value="updateSlider" />
-        <div flex items-center justify-between text-14px  mt-6px>
-          <span text="#4343ef">{{currentTimeFormat}}</span>
-          <span>{{durationFormat}}</span>
+        <img rounded-16px w="200px" h="120px" inline-block blur="0.5" :src="playListStore.selectItem?.al?.picUrl"
+          v-if="playListStore.selectItem?.al?.picUrl" />
+        <div text="#24242e dark:white 24px" font-700 my-4px>{{ playListStore.selectItem?.name }}</div>
+        <div text="#4d4d56 16px" font-500 mb-8px>{{ playListStore.selectItem?.ar?.map((p: any) => p.name).join('/') }}</div>
+        <n-slider :tooltip="false" v-model:value="slider" :on-update:value="updateSlider" />
+        <div flex items-center justify-between text-14px mt-6px>
+          <span text="#4343ef">{{ currentTimeFormat }}</span>
+          <span>{{ durationFormat }}</span>
         </div>
 
-        <div flex items-center w="100%" justify-between >
-          <n-popover trigger="hover" :show-arrow="false">
-            <template #trigger>
-              <i icon-btn  :class="[playModelIcon]" text="#b4b4f8 20px" @click="changeModel"></i>
-            </template>
-              <span>{{playModelFormat}}</span>
-          </n-popover>
+        <div flex items-center w="100%">
+          <div basis="1/4" flex items-center>
+            <n-popover trigger="hover" :show-arrow="false">
+              <template #trigger>
+                <i icon-btn :class="[playModelIcon]" text="#b4b4f8 20px" @click="changeModel"></i>
+              </template>
+              <span>{{ playModelFormat }}</span>
+            </n-popover>
+
+            <n-popover trigger="hover" :show-arrow="false" style=" height: 160px">
+              <template #trigger>
+                <i icon-btn :class="[playModelIcon]" text="#b4b4f8 20px"></i>
+              </template>
+              <n-slider h="100%" style="height: 160px;" :tooltip="false" vertical />
+            </n-popover>
+
+          </div>
 
 
-          <div text="#4343ef"  flex items-center min-w="150px" justify-between>
+          <div text="#4343ef" flex items-center min-w="150px" justify-between basis="1/2">
             <i icon-btn i-ic:baseline-skip-previous text-28px @click="playListStore.previous"></i>
             <div w="42px" h="42px" bg="#4343ef" rounded-full flex items-center justify-center @click="changePlay">
-              <i icon-btn  text="white 25px"  :class="[playIcon]" ></i>
+              <i icon-btn text="white 25px" :class="[playIcon]"></i>
             </div>
             <i icon-btn i-ic:baseline-skip-next text-28px @click="playListStore.next"></i>
           </div>
-          <n-popover trigger="hover" :show-arrow="false">
-            <template #trigger>
-              <i icon-btn  text="#b4b4f8 20px" i-ic:baseline-format-list-bulleted ></i>
-            </template>
+
+          <div basis="1/4">
+            <n-popover trigger="hover" :show-arrow="false">
+              <template #trigger>
+                <i icon-btn text="#b4b4f8 20px" i-ic:baseline-format-list-bulleted></i>
+              </template>
               <span>播放列表</span>
-          </n-popover>
-         
+            </n-popover>
+          </div>
+
         </div>
-        <div  text-center mt20px>
-          <i icon-btn  text="#b3b3bc 20px" i-ic:twotone-expand-less inline-block></i>
-          <div text="#787887 15px"  font-500>Lyrics</div>
+        <div text-center mt20px>
+          <i icon-btn text="#b3b3bc 20px" i-ic:twotone-expand-less inline-block></i>
+          <div text="#787887 15px" font-500>Lyrics</div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-
-
-:global(.n-slider .n-slider-handles .n-slider-handle-wrapper .n-slider-handle) {
-  
-}
-
-:global(.n-slider .n-slider-handles:hover .n-slider-handle){
-  visibility:visible
-}
-
-
 </style>
---
+++ src/pages/hi/[name].vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { m } from 'vitest/dist/index-5aad25c1'
+
+const showModal = ref(false)
+const list = ref(
+  [
+    {
+      label: 'A',
+      id: `${new Date()}`,
+      children: [
+        {},
+        {},
+      ],
+    },
+
+  ],
+)
+
+const options = [
+  {
+    label: 'A',
+    value: 'A',
+  },
+  {
+    label: 'B',
+    value: 'B',
+  },
+  {
+    label: 'C',
+    value: 'C',
+  },
+]
+
+const selected = ref('A') // 所选的LABEL
+
+const selectItem = ref<{
+  label: string
+  id: string
+  children: {}[]
+}>() // 所选的Item
+const selectIndex = ref(0) // 点击的item children的index
+function add(item: {
+  label: string
+  id: string
+  children: {}[]
+}, k: number) {
+  showModal.value = !showModal.value
+  selectItem.value = item
+  selectIndex.value = k
+}
+// 点击保存按钮
+function save() {
+  const index = list.value.findIndex(l => l.id === selectItem.value?.id)
+  console.log(selectItem.value?.id)
+  
+
+  // 如果点击相同
+  if (selectItem.value!.label === selected.value) { // 找到了
+    if (~index)
+      list.value[index].children.push({})
+  }
+  else {
+    // 点击添加不同
+    // 判断是否是最后一个
+
+    if (selectIndex.value === list.value[index].children.length - 1) {
+      list.value.push({
+        label: selected.value,
+        id: `${new Date()}${new Date()}`,
+        children: [{}],
+      })
+    }
+    else {
+      const len = list.value[index].children.length
+      const tempChildren = list.value[index].children.slice(selectIndex.value + 1, len)
+      list.value[index].children = list.value[index].children.slice(0, selectIndex.value + 1)
+      list.value.splice(index + 1, 0, {
+        label: selected.value,
+        id: `${new Date()}`,
+        children: [{}],
+      })
+      list.value.splice(index + 2, 0, {
+        label: list.value[index].label,
+        id: `${new Date()}`,
+        children: [...tempChildren],
+      })
+    }
+  }
+  showModal.value = !showModal.value
+}
+</script>
+
+<template>
+  <n-modal v-model:show="showModal">
+    <div w="240px" text-center>
+      <n-select v-model:value="selected" :options="options" />
+      <button btn mt-30px @click="save">
+        保存
+      </button>
+    </div>
+  </n-modal>
+  <div v-for="item in list" :key="item.id">
+    {{ item.label }}
+    <div v-for="_,j in item.children" :key="j" w="240px" flex items-center justify-between my-10px>
+      第{{ j+1 }}项
+      <div>
+        <button btn mr-10px @click="add(item,j)">
+          添加
+        </button>
+        <button btn>
+          删除
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
